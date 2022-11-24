d7b0262deda8100e429371962b79db6174b740d6
AlbanMarku <albanmarku@outlook.com>
2022-11-24 15:33:11 +0000

    type fix

--- server/route.js
+++ server/route.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const multer = require("multer");
-// const { readFileSync } = require("fs");
-// const uniqid = require("uniqid");
+const { readFileSync } = require("fs");
+const uniqid = require("uniqid");
 const Item = require("./models/item");
-// const fireApp = require("./firebase.config");
+const fireApp = require("./firebase.config");
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
@@ -21,31 +21,32 @@ const endpoint = (app) => {
     res.json({ message: "👋 from Express!" });
   });
 
-  app.post("/api/upload", upload.single("image"), async () => {
-    // const imageRef = fireApp.storage.ref(
-    //   fireApp.fireStorage,
-    //   `imgs/${req.file.filename}/`
-    // );
-    // try {
-    //   await fireApp.storage.uploadBytesResumable(
-    //     imageRef,
-    //     readFileSync(`imgs/${req.file.filename}`)
-    //   );
-    //   const imageUrl = await fireApp.storage.getDownloadURL(imageRef);
-    //   const item = new Item({
-    //     name: "theName",
-    //     imageLink: imageUrl,
-    //     id: uniqid("image-"),
-    //   });
-    //   try {
-    //     const savedResp = await item.save();
-    //     res.json({ message: savedResp });
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // } catch (err) {
-    //   console.log(err);
-    // }
+  app.post("/api/upload", upload.single("image"), async (req, res) => {
+    const imageRef = fireApp.storage.ref(
+      fireApp.fireStorage,
+      `imgs/${req.file.filename}/`
+    );
+    try {
+      await fireApp.storage.uploadBytesResumable(
+        imageRef,
+        readFileSync(`imgs/${req.file.filename}`)
+      );
+      const imageUrl = await fireApp.storage.getDownloadURL(imageRef);
+
+      const item = new Item({
+        name: "theName",
+        imageLink: imageUrl,
+        id: uniqid("image-"),
+      });
+      try {
+        const savedResp = await item.save();
+        res.json({ message: savedResp });
+      } catch (err) {
+        console.log(err);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   app.get("/api/fetchAll", async (req, res) => {

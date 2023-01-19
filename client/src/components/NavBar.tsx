fa898372d7d2db7650f95c878cc53fb7b976bb45
AlbanMarku <albanmarku@outlook.com>
2023-01-19 16:44:58 +0000

    Hamburger menu.

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navBar.css';
 
@@ -6,23 +7,40 @@ type Props = {
 };
 
 function NavBar({ user }: Props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link className="link" to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="link" to="/manage">
-            Manage
-          </Link>
-        </li>
-      </ul>
-      <h1>Inventory</h1>
-      <div>{user ? <p>Welcome {user}</p> : <p>Not signed in</p>}</div>
-    </nav>
+    <div className="NavBar">
+      <nav className="desktopNav">
+        <div className="navTitle">
+          <h1>Inventory</h1>
+          <div>{user ? <p>Welcome {user}</p> : <p>Not signed in</p>}</div>
+        </div>
+        <ul className={`menu-list ${menuOpen ? 'open' : ''}`}>
+          <li>
+            <Link onClick={handleToggle} className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleToggle} className="link" to="/manage">
+              Manage
+            </Link>
+          </li>
+        </ul>
+
+        <button
+          onClick={handleToggle}
+          className={`hamburger ${menuOpen ? 'active' : ''}`}
+          type="button"
+        >
+          p
+        </button>
+      </nav>
+    </div>
   );
 }
 
